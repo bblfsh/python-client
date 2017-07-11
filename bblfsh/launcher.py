86075d522dcf71ba003d15233b5ddb418a4025a1
Vadim Markovtsev <gmarkhor@gmail.com>
2017-07-11 10:31:24 +0200

    Remove redundant \n in logs

--- bblfsh/launcher.py
+++ bblfsh/launcher.py
@@ -20,13 +20,13 @@ def ensure_bblfsh_is_running():
         )
         log.warning(
             "Launched the Babelfish server (name bblfsh, id %s).\nStop it "
-            "with: docker rm -f bblfsh\n", container.id)
+            "with: docker rm -f bblfsh", container.id)
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
             result = -1
             while result != 0:
                 time.sleep(0.1)
                 result = sock.connect_ex(("0.0.0.0", 9432))
-        log.warning("Babelfish server is up and running.\n")
+        log.warning("Babelfish server is up and running.")
         return False
     finally:
         client.api.close()
