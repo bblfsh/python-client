a0f5bd8a2c73319162c6238312f2aa8faded03bc
Juanjo Alvarez <juanjo@sourced.tech>
2017-11-02 18:01:47 +0100

    Added "native_parse" and "version" requests.
    Signed-off-by: Juanjo Alvarez <juanjo@sourced.tech>

--- bblfsh/client.py
+++ bblfsh/client.py
@@ -1,3 +1,4 @@
+from ast import literal_eval
 import os
 import sys
 import importlib
@@ -62,6 +63,51 @@ class BblfshClient(object):
                                content=contents,
                                language=self._scramble_language(language))
         return self._stub.Parse(request, timeout=timeout)
+
+    def native_parse(self, filename, language=None, contents=None, timeout=None):
+        """
+        Queries the Babelfish server and receives the native AST response for the specified
+        file.
+
+        :param filename: The path to the file. Can be arbitrary if contents \
+                         is not None.
+        :param language: The programming language of the file. Refer to \
+                         https://doc.bblf.sh/languages.html for the list of \
+                         currently supported languages. None means autodetect.
+        :param contents: The contents of the file. IF None, it is read from \
+                         filename.
+        :param timeout: The request timeout in seconds.
+        :type filename: str
+        :type language: str
+        :type contents: str
+        :type timeout: float
+        :return: Native AST object.
+        """
+
+        NativeParseRequest = importlib.import_module(
+                "bblfsh.gopkg.in.bblfsh.sdk.%s.protocol.generated_pb2" % VERSION
+                ).NativeParseRequest
+
+        if contents is None:
+            with open(filename, "rb") as fin:
+                contents = fin.read()
+        request = NativeParseRequest(filename=os.path.basename(filename),
+                               content=contents,
+                               language=self._scramble_language(language))
+        return self._stub.NativeParse(request, timeout=timeout)
+
+    def version(self):
+        """
+        Queries the Babelfish server for version and runtime information.
+
+        :return: A dictionary with the keys "version" for the semantic version and
+                 "build" for the build timestamp.
+        """
+        VersionRequest = importlib.import_module(
+                "bblfsh.gopkg.in.bblfsh.sdk.%s.protocol.generated_pb2" % VERSION
+                ).VersionRequest
+
+        return self._stub.Version(VersionRequest())
 
     @staticmethod
     def _scramble_language(lang):
