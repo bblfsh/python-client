e30b4ab4ed69599f3814f40fd8c1b18322e5bd92
Juanjo Alvarez <juanjo@sourced.tech>
2017-12-22 14:33:33 +0100

    Version 2.8.0
    Signed-off-by: Juanjo Alvarez <juanjo@sourced.tech>

--- setup.py
+++ setup.py
@@ -134,7 +134,7 @@ def main():
         },
         name="bblfsh",
         description="Fetches Universal Abstract Syntax Trees from Babelfish.",
-        version="2.7.0",
+        version="2.8.0",
         license="Apache 2.0",
         author="source{d}",
         author_email="language-analysis@sourced.tech",
