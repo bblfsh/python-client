fe1ecca588aa8219b140eba53bcc9e4e68c09c44
Juanjo Alvarez <juanjo@sourced.tech>
2018-06-01 09:40:54 +0200

    HasAttribute: Ensure no py_decref is called on a null pointer
    Signed-off-by: Juanjo Alvarez <juanjo@sourced.tech>
    
    Increase version
    
    Signed-off-by: Juanjo Alvarez <juanjo@sourced.tech>

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 from setuptools import setup, find_packages, Extension
 from setuptools.command.build_ext import build_ext
 
-VERSION = "2.9.12"
+VERSION = "2.9.13"
 LIBUAST_VERSION = "v1.9.1"
 SDK_VERSION = "v1.8.0"
 SDK_MAJOR = SDK_VERSION.split('.')[0]
