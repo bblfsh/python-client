6474dd774799909d3de68d665342f4b7f2d945f1
Vadim Markovtsev <vadim@sourced.tech>
2018-07-25 15:51:51 +0200

    Bump grpcio version
    Signed-off-by: Vadim Markovtsev <vadim@sourced.tech>

--- setup.py
+++ setup.py
@@ -152,7 +152,7 @@ def main():
         packages=find_packages(),
         exclude=["bblfsh/test.py"],
         keywords=["babelfish", "uast"],
-        install_requires=["grpcio==1.10.0", "grpcio-tools==1.10.0", "docker", "protobuf>=3.4.0"],
+        install_requires=["grpcio==1.13.0", "grpcio-tools==1.13.0", "docker", "protobuf>=3.4.0"],
         package_data={"": ["LICENSE", "README.md"]},
         ext_modules=[libuast_module],
         classifiers=[
