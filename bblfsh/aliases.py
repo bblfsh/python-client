555379af3b92e1b6b578393bbf99761bce66dc7e
Vadim Markovtsev <vadim@sourced.tech>
2018-09-04 13:30:11 +0200

    Fix PB imports (#122)
    Changes:
    
    1. Place pb files inside proto
    2. Fix the generated grpc stubs. The grpc plugin is crazy and ignores ".in." in imports, it also does not respect the directory structure.
    3. Prepend "bblfsh" to all the imports in the bindings
    4. Remove the sys.path hacks
    5. Add `isinstance` test
    
    So now we have absolute imports starting with "bblfsh" **everywhere** and no classes are duplicated.
    
    Signed-off-by: Vadim Markovtsev <vadim@sourced.tech>

--- bblfsh/aliases.py
+++ bblfsh/aliases.py
@@ -3,15 +3,8 @@ __all__ = ["DESCRIPTOR", "Node", "ParseResponse", "NativeParseResponse",
            "ProtocolServiceStub"]
 
 import importlib
-import os
-import sys
 
 from bblfsh.sdkversion import VERSION
-
-# The following two insertions fix the broken pb import paths
-sys.path.insert(0, os.path.join(os.path.dirname(__file__),
-                                "gopkg/in/bblfsh/sdk/%s/protocol" % VERSION))
-sys.path.insert(0, os.path.dirname(__file__))
 
 # "in" is a reserved keyword in Python thus can't be used as package name, so
 # we import by string
