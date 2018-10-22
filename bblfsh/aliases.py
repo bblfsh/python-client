0c84421820e08d13fb201a47af3a609903f5dc0c
Vadim Markovtsev <vadim@sourced.tech>
2018-10-22 17:22:00 +0200

    Refactor aliases
    Signed-off-by: Vadim Markovtsev <vadim@sourced.tech>

--- bblfsh/aliases.py
+++ bblfsh/aliases.py
@@ -1,6 +1,5 @@
-__all__ = ["DESCRIPTOR", "Node", "ParseResponse", "NativeParseResponse",
-           "ParseRequest", "NativeParseRequest", "VersionRequest",
-           "ProtocolServiceStub"]
+__all__ = ["DESCRIPTOR", "Node", "Position", "ParseResponse", "NativeParseResponse",
+           "ParseRequest", "NativeParseRequest", "VersionRequest", "ProtocolServiceStub"]
 
 import importlib
 
@@ -8,39 +7,21 @@ from bblfsh.sdkversion import VERSION
 
 # "in" is a reserved keyword in Python thus can't be used as package name, so
 # we import by string
+uast_module = importlib.import_module(
+    "bblfsh.gopkg.in.bblfsh.sdk.%s.uast.generated_pb2" % VERSION)
+protocol_module = importlib.import_module(
+    "bblfsh.gopkg.in.bblfsh.sdk.%s.protocol.generated_pb2" % VERSION)
+protocol_grpc_module = importlib.import_module(
+    "bblfsh.gopkg.in.bblfsh.sdk.%s.protocol.generated_pb2_grpc" % VERSION)
 
-DESCRIPTOR = importlib.import_module(
-        "bblfsh.gopkg.in.bblfsh.sdk.%s.uast.generated_pb2" % VERSION).DESCRIPTOR
-
-Node = importlib.import_module(
-        "bblfsh.gopkg.in.bblfsh.sdk.%s.uast.generated_pb2" % VERSION).Node
-
-ParseResponse = importlib.import_module(
-    "bblfsh.gopkg.in.bblfsh.sdk.%s.protocol.generated_pb2" % VERSION).ParseResponse
-
-NativeParseResponse = importlib.import_module(
-        "bblfsh.gopkg.in.bblfsh.sdk.%s.protocol.generated_pb2" % VERSION
-        ).NativeParseResponse
-
-ParseRequest = importlib.import_module(
-    "bblfsh.gopkg.in.bblfsh.sdk.%s.protocol.generated_pb2" % VERSION).ParseRequest
-
-NativeParseRequest = importlib.import_module(
-        "bblfsh.gopkg.in.bblfsh.sdk.%s.protocol.generated_pb2" % VERSION
-        ).NativeParseRequest
-
-VersionRequest = importlib.import_module(
-        "bblfsh.gopkg.in.bblfsh.sdk.%s.protocol.generated_pb2" % VERSION
-        ).VersionRequest
-
-SupportedLanguagesRequest = importlib.import_module(
-        "bblfsh.gopkg.in.bblfsh.sdk.%s.protocol.generated_pb2" % VERSION
-        ).SupportedLanguagesRequest
-
-SupportedLanguagesResponse = importlib.import_module(
-        "bblfsh.gopkg.in.bblfsh.sdk.%s.protocol.generated_pb2" % VERSION
-        ).SupportedLanguagesResponse
-
-ProtocolServiceStub = importlib.import_module(
-        "bblfsh.gopkg.in.bblfsh.sdk.%s.protocol.generated_pb2_grpc" % VERSION
-        ).ProtocolServiceStub
+DESCRIPTOR = uast_module.DESCRIPTOR
+Node = uast_module.Node
+Position = uast_module.Position
+ParseResponse = protocol_module.ParseResponse
+NativeParseResponse = protocol_module.NativeParseResponse
+ParseRequest = protocol_module.ParseRequest
+NativeParseRequest = protocol_module.NativeParseRequest
+VersionRequest = protocol_module.VersionRequest
+SupportedLanguagesRequest = protocol_module.SupportedLanguagesRequest
+SupportedLanguagesResponse = protocol_module.SupportedLanguagesResponse
+ProtocolServiceStub = protocol_grpc_module.ProtocolServiceStub
