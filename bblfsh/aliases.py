7f583ea0de7be4652b3599b4c7521d9e056935ed
Juanjo Alvarez <juanjo@sourced.tech>
2018-10-22 19:19:51 +0200

    Forward port the aliases refactor by Vadim
    Signed-off-by: Juanjo Alvarez <juanjo@sourced.tech>

--- bblfsh/aliases.py
+++ bblfsh/aliases.py
@@ -5,44 +5,24 @@ from bblfsh.sdkversion import VERSION
 # "in" is a reserved keyword in Python thus can't be used as package name, so
 # we import by string
 
-DESCRIPTOR = importlib.import_module(
-        "bblfsh.gopkg.in.bblfsh.sdk.v2.uast.generated_pb2").DESCRIPTOR
+uast_v2_module = importlib.import_module("bblfsh.gopkg.in.bblfsh.sdk.v2.uast.generated_pb2")
+protocol_v2_module = importlib.import_module("bblfsh.gopkg.in.bblfsh.sdk.v2.protocol.generated_pb2")
+protocol_grpc_v2_module = importlib.import_module("bblfsh.gopkg.in.bblfsh.sdk.v2.protocol.generated_pb2_grpc")
+protocol_v1_module = importlib.import_module("bblfsh.gopkg.in.bblfsh.sdk.v1.protocol.generated_pb2")
+protocol_grpc_v1_module = importlib.import_module("bblfsh.gopkg.in.bblfsh.sdk.v1.protocol.generated_pb2_grpc")
 
 # Node = importlib.import_module(
         # "bblfsh.gopkg.in.bblfsh.sdk.v2.uast.generated_pb2").Node
 
-ParseResponse = importlib.import_module(
-    "bblfsh.gopkg.in.bblfsh.sdk.v2.protocol.generated_pb2").ParseResponse
+DESCRIPTOR = uast_v2_module.DESCRIPTOR
+ParseRequest = protocol_v2_module.ParseRequest
+ParseResponse = protocol_v2_module.ParseResponse
+ParseError = protocol_v2_module.ParseError
+Mode = protocol_v2_module.Mode
+DriverStub = protocol_grpc_v2_module.DriverStub
+DriverServicer = protocol_grpc_v2_module.DriverServicer
 
-ParseError = importlib.import_module(
-    "bblfsh.gopkg.in.bblfsh.sdk.v2.protocol.generated_pb2").ParseError
-
-Mode = importlib.import_module(
-    "bblfsh.gopkg.in.bblfsh.sdk.v2.protocol.generated_pb2").Mode
-
-ParseRequest = importlib.import_module(
-    "bblfsh.gopkg.in.bblfsh.sdk.v2.protocol.generated_pb2").ParseRequest
-
-VersionRequest = importlib.import_module(
-        "bblfsh.gopkg.in.bblfsh.sdk.v1.protocol.generated_pb2"
-        ).VersionRequest
-
-SupportedLanguagesRequest = importlib.import_module(
-        "bblfsh.gopkg.in.bblfsh.sdk.v1.protocol.generated_pb2"
-        ).SupportedLanguagesRequest
-
-SupportedLanguagesResponse = importlib.import_module(
-        "bblfsh.gopkg.in.bblfsh.sdk.v1.protocol.generated_pb2"
-        ).SupportedLanguagesResponse
-
-ProtocolServiceStub = importlib.import_module(
-        "bblfsh.gopkg.in.bblfsh.sdk.v1.protocol.generated_pb2_grpc"
-        ).ProtocolServiceStub
-
-DriverStub = importlib.import_module(
-        "bblfsh.gopkg.in.bblfsh.sdk.v2.protocol.generated_pb2_grpc"
-        ).DriverStub
-
-DriverServicer = importlib.import_module(
-        "bblfsh.gopkg.in.bblfsh.sdk.v2.protocol.generated_pb2_grpc"
-        ).DriverServicer
+VersionRequest = protocol_v1_module.VersionRequest
+SupportedLanguagesRequest = protocol_v1_module.SupportedLanguagesRequest
+SupportedLanguagesResponse = protocol_v1_module.SupportedLanguagesResponse
+ProtocolServiceStub = protocol_grpc_v1_module.ProtocolServiceStub
