f289d09de7ebf1d950b42514a04d82b5a4c1abee
Juanjo Alvarez Martinez <juanjo@juanjoalvarez.net>
2018-10-23 14:36:37 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from urllib.request import urlopen
 from setuptools import setup, find_packages, Extension
 from setuptools.command.build_ext import build_ext
 
-VERSION = "2.12.2"
+VERSION = "2.12.3"
 LIBUAST_VERSION = "v1.9.5"
 SDK_VERSION = "v1.16.1"
 SDK_MAJOR = SDK_VERSION.split('.')[0]
