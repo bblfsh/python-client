3f34176528e036dfe82fe4df597ffffdf18cbccf
ncordon <nacho.cordon.castillo@gmail.com>
2019-08-02 20:57:41 +0200

    Makes the tree orders constants instead of wrapping them
    Signed-off-by: ncordon <nacho.cordon.castillo@gmail.com>

--- bblfsh/tree_order.py
+++ bblfsh/tree_order.py
@@ -1,16 +1,16 @@
 from enum import IntEnum
-from bblfsh.pyuast import TreeOrder
+from bblfsh.pyuast import AnyOrder, PreOrder, PostOrder, ChildrenOrder, LevelOrder, PositionOrder
 
 class TreeOrder(IntEnum):
     # Gives no assurances over the iteration order of the tree
     # Uses the fastest one available (the one that the tree
     # natively supports)
-    ANY_ORDER      = TreeOrder.ANY_ORDER()
-    PRE_ORDER      = TreeOrder.PRE_ORDER()
-    POST_ORDER     = TreeOrder.POST_ORDER()
-    LEVEL_ORDER    = TreeOrder.LEVEL_ORDER()
-    CHILDREN_ORDER = TreeOrder.CHILDREN_ORDER()
-    POSITION_ORDER = TreeOrder.POSITION_ORDER()
+    ANY_ORDER      = AnyOrder
+    PRE_ORDER      = PreOrder
+    POST_ORDER     = PostOrder
+    LEVEL_ORDER    = LevelOrder
+    CHILDREN_ORDER = ChildrenOrder
+    POSITION_ORDER = PositionOrder
 
     @staticmethod
     def check_order(order: int) -> None:
