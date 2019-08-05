9f8ce711bf9d10bc90136d93168d9059d6af5f65
ncordon <nacho.cordon.castillo@gmail.com>
2019-08-05 20:06:26 +0200

    Rolls back adding the ChildrenOrder
    Signed-off-by: ncordon <nacho.cordon.castillo@gmail.com>

--- bblfsh/tree_order.py
+++ bblfsh/tree_order.py
@@ -1,5 +1,5 @@
 from enum import IntEnum
-from bblfsh.pyuast import AnyOrder, PreOrder, PostOrder, ChildrenOrder, LevelOrder, PositionOrder
+from bblfsh.pyuast import AnyOrder, PreOrder, PostOrder, LevelOrder, PositionOrder
 
 class TreeOrder(IntEnum):
     # Gives no assurances over the iteration order of the tree
@@ -9,7 +9,6 @@ class TreeOrder(IntEnum):
     PRE_ORDER      = PreOrder
     POST_ORDER     = PostOrder
     LEVEL_ORDER    = LevelOrder
-    CHILDREN_ORDER = ChildrenOrder
     POSITION_ORDER = PositionOrder
 
     @staticmethod
